6e6b234cf53d234f875e6c150afbe20a54e4c755
DESKTOP-9D52CCT\付建森 <1>
2024-03-24 00:20:19 +0800

    feat: 登录界面

--- common/api.js
+++ common/api.js
@@ -1,21 +1,70 @@
+// 假设你的api.js文件中的代码类似于这样
+export const baseUrl = "http://127.0.0.1:8081";
 
-export const baseUel="http://127.0.0.1:15087/"
-
-export default{
-    
-    post(url,param){
-        return new Promise((resolve,reject) =>{
+export default {
+    // 通用请求方法，增加了401处理逻辑
+    request(method, url, param) {
+        return new Promise((resolve, reject) => {
+            const token = uni.getStorageSync('token');
+            const headers = {
+                'Content-Type': 'application/json',
+            };
+            if (token) {
+                headers['Authorization'] = `Bearer ${token}`;
+            }
             uni.request({
-                method:'POST',
-                url: baseUel+url,
+                method: method,
+                url: baseUrl + url,
                 data: param,
-                header:{
-					'Content-Type': 'application/json'
-				}
-            }).then(res=>{
-				   resolve(res[1].data)
-				  reject(res)
-			})
-        })        
+                header: headers,
+            }).then(res => {
+                const [_, response] = res;
+                // 检查是否返回了401状态码
+                if (response.statusCode === 401) {
+                    uni.showToast({
+                        title: '登录过期，请重新登录',
+                        icon: 'none',
+                        duration: 2000,
+                        complete: () => {
+                            uni.navigateTo({ url: '/pages/public/login' });
+                        }
+                    });
+                    reject(new Error('登录过期，请重新登录'));
+                } else if (response.statusCode === 200) {
+                    // 状态码为200，正常处理数据
+                    resolve(response.data);
+                } else {
+                    // 对于所有其他状态码，展示后端返回的消息
+                    uni.showToast({
+                        title: response.data.message || '发生错误，请稍后重试',
+                        icon: 'none',
+                        duration: 2000
+                    });
+                    reject(new Error(response.data.message || '发生错误，请稍后重试'));
+                }
+            }).catch(err => {
+                // 对于请求发送失败的情况，直接reject错误
+                uni.showToast({
+                    title: '请求失败，请检查网络设置',
+                    icon: 'none',
+                    duration: 2000
+                });
+                reject(err);
+            });
+        });
     },
-}
+    // 封装GET请求
+    get(url, params) {
+        return this.request('GET', url, params);
+    },
+    // 封装POST请求
+    post(url, params) {
+        return this.request('POST', url, params);
+    },
+    put(url, params) {
+        return this.request('PUT', url, params);
+    },
+    delete(url, params) {
+        return this.request('DELETE', url, params);
+    },
+};
--- pages/public/login.vue
+++ pages/public/login.vue
@@ -12,28 +12,13 @@
 			<view class="input-content">
 				<view class="input-item">
 					<text class="tit">手机号码</text>
-					<input 
-						type="number" 
-						:value="mobile" 
-						placeholder="请输入手机号码"
-						maxlength="11"
-						data-key="mobile"
-						@input="inputChange"
-					/>
+					<input type="number" :value="mobile" placeholder="请输入手机号码" maxlength="11" data-key="mobile"
+						@input="inputChange" />
 				</view>
 				<view class="input-item">
 					<text class="tit">密码</text>
-					<input 
-						type="mobile" 
-						value="" 
-						placeholder="8-18位不含特殊字符的数字、字母组合"
-						placeholder-class="input-empty"
-						maxlength="20"
-						password 
-						data-key="password"
-						@input="inputChange"
-						@confirm="toLogin"
-					/>
+					<input type="mobile" value="" placeholder="8-18位不含特殊字符的数字、字母组合" placeholder-class="input-empty"
+						maxlength="20" password data-key="password" @input="inputChange" @confirm="toLogin" />
 				</view>
 			</view>
 			<button class="confirm-btn" @click="toLogin" :disabled="logining">登录</button>
@@ -49,83 +34,99 @@
 </template>
 
 <script>
-	import {  
-        mapMutations  
-    } from 'vuex';
-	
-	export default{
-		data(){
+	import {
+		mapMutations
+	} from 'vuex';
+
+	import api from '../../common/api.js'
+
+	export default {
+		data() {
 			return {
 				mobile: '',
 				password: '',
 				logining: false
 			}
 		},
-		onLoad(){
-			
+		onLoad() {
+
 		},
 		methods: {
 			...mapMutations(['login']),
-			inputChange(e){
+			inputChange(e) {
 				const key = e.currentTarget.dataset.key;
 				this[key] = e.detail.value;
 			},
-			navBack(){
+			navBack() {
 				uni.navigateBack();
 			},
-			toRegist(){
+			toRegist() {
 				this.$api.msg('去注册');
 			},
-			async toLogin(){
-				this.logining = true;
-				const {mobile, password} = this;
-				/* 数据验证模块
-				if(!this.$api.match({
+			async toLogin() {
+				const {
 					mobile,
 					password
-				})){
-					this.logining = false;
-					return;
-				}
-				*/
+				} = this;
+				/* 数据验证省略 */
+
 				const sendData = {
-					mobile,
-					password
+					code: mobile, // 后端期望的是code字段
+					password: password
 				};
-				const result = await this.$api.json('userInfo');
-				if(result.status === 1){
-					this.login(result.data);
-                    uni.navigateBack();  
-				}else{
-					this.$api.msg(result.msg);
-					this.logining = false;
+
+				// 替换为实际的API调用
+				const result = await api.post('/mirageLedger/user/login',
+				sendData); // 假设$this.$api.post已正确设置来发送HTTP请求
+				if (result.token && result.userInfo) {
+					this.logining = true;
+					// 存储token和用户信息
+					uni.setStorageSync("token", result.token);
+					uni.setStorageSync("userInfo", JSON.stringify(result.userInfo));
+				
+					// 登录成功，跳转或其他操作
+					uni.showToast({
+						title: '登录成功',
+						duration: 2000,
+						success: () => {
+							uni.navigateTo({ url: '/pages/index/index' });
+						}
+					});
+				} else {
+					// 处理登录失败
+					this.$api.msg('登录失败');
 				}
+				this.logining = true;
 			}
+
 		},
 
 	}
 </script>
 
 <style lang='scss'>
-	page{
+	page {
 		background: #fff;
 	}
-	.container{
+
+	.container {
 		padding-top: 115px;
-		position:relative;
+		position: relative;
 		width: 100vw;
 		height: 100vh;
 		overflow: hidden;
 		background: #fff;
 	}
-	.wrapper{
-		position:relative;
+
+	.wrapper {
+		position: relative;
 		z-index: 90;
 		background: #fff;
 		padding-bottom: 40upx;
 	}
-	.back-btn{
-		position:absolute;
+
+	.back-btn {
+		position: absolute;
 		left: 40upx;
 		z-index: 9999;
 		padding-top: var(--status-bar-height);
@@ -133,29 +134,35 @@
 		font-size: 40upx;
 		color: $font-color-dark;
 	}
-	.left-top-sign{
+
+	.left-top-sign {
 		font-size: 120upx;
 		color: $page-color-base;
-		position:relative;
+		position: relative;
 		left: -16upx;
 	}
-	.right-top-sign{
-		position:absolute;
+
+	.right-top-sign {
+		position: absolute;
 		top: 80upx;
 		right: -30upx;
 		z-index: 95;
-		&:before, &:after{
-			display:block;
-			content:"";
+
+		&:before,
+		&:after {
+			display: block;
+			content: "";
 			width: 400upx;
 			height: 80upx;
 			background: #b4f3e2;
 		}
-		&:before{
+
+		&:before {
 			transform: rotate(50deg);
 			border-radius: 0 50px 0 0;
 		}
-		&:after{
+
+		&:after {
 			position: absolute;
 			right: -198upx;
 			top: 0;
@@ -164,53 +171,60 @@
 			/* background: pink; */
 		}
 	}
-	.left-bottom-sign{
-		position:absolute;
+
+	.left-bottom-sign {
+		position: absolute;
 		left: -270upx;
 		bottom: -320upx;
 		border: 100upx solid #d0d1fd;
 		border-radius: 50%;
 		padding: 180upx;
 	}
-	.welcome{
-		position:relative;
+
+	.welcome {
+		position: relative;
 		left: 50upx;
 		top: -90upx;
 		font-size: 46upx;
 		color: #555;
-		text-shadow: 1px 0px 1px rgba(0,0,0,.3);
+		text-shadow: 1px 0px 1px rgba(0, 0, 0, .3);
 	}
-	.input-content{
+
+	.input-content {
 		padding: 0 60upx;
 	}
-	.input-item{
-		display:flex;
+
+	.input-item {
+		display: flex;
 		flex-direction: column;
-		align-items:flex-start;
+		align-items: flex-start;
 		justify-content: center;
 		padding: 0 30upx;
-		background:$page-color-light;
+		background: $page-color-light;
 		height: 120upx;
 		border-radius: 4px;
 		margin-bottom: 50upx;
-		&:last-child{
+
+		&:last-child {
 			margin-bottom: 0;
 		}
-		.tit{
+
+		.tit {
 			height: 50upx;
 			line-height: 56upx;
 			font-size: $font-sm+2upx;
 			color: $font-color-base;
 		}
-		input{
+
+		input {
 			height: 60upx;
 			font-size: $font-base + 2upx;
 			color: $font-color-dark;
 			width: 100%;
-		}	
+		}
 	}
 
-	.confirm-btn{
+	.confirm-btn {
 		width: 630upx;
 		height: 76upx;
 		line-height: 76upx;
@@ -219,25 +233,29 @@
 		background: $uni-color-primary;
 		color: #fff;
 		font-size: $font-lg;
-		&:after{
+
+		&:after {
 			border-radius: 100px;
 		}
 	}
-	.forget-section{
+
+	.forget-section {
 		font-size: $font-sm+2upx;
 		color: $font-color-spec;
 		text-align: center;
 		margin-top: 40upx;
 	}
-	.register-section{
-		position:absolute;
+
+	.register-section {
+		position: absolute;
 		left: 0;
 		bottom: 50upx;
 		width: 100%;
 		font-size: $font-sm+2upx;
 		color: $font-color-base;
 		text-align: center;
-		text{
+
+		text {
 			color: $font-color-spec;
 			margin-left: 10upx;
 		}
